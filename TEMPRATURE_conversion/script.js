15b059b59138f7b2fe82df561fd2f528070d50c2
DHAMI-HARSH <158556321+DHAMI-HARSH@users.noreply.github.com>
2024-06-27 20:29:19 +0530

    this is my first commit

---
+++ TEMPRATURE_conversion/script.js
@@ -0,0 +1,23 @@
+const number = document.getElementById("number");
+const fahrenheit = document.getElementById("toFahrenheit"); 
+const celsius = document.getElementById("toCelsius"); 
+const resultElement = document.getElementById("result"); 
+let temp;
+
+function convert(){
+    if(fahrenheit.checked){
+        temp = (number.value * 9/5) + 32;
+        resultElement.textContent=temp
+         
+
+
+    }
+    else if(celsius.checked){
+        temp = (number.value - 32) * 5/9;
+        resultElement.textContent=temp
+    }
+    else{
+        alert("Please select a unit");
+    }
+
+}
